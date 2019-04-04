60557d1cf58277b88e2f6c48e8a7ac8143121f7f
chlqudrnr1 <chlqudrnr@gmail.com>
2019-04-04 09:52:25 +0900

    등록

--- server/routes/user/index/indexmanage.js
+++ server/routes/user/index/indexmanage.js
@@ -17,10 +17,11 @@ var getInfoOpenReqList = function (req, res) {
     console.log('indexmanage 모듈 안에 있는 getInfoOpenReqList 호출됨.');
 
     var pool = req.app.get("pool");
-    var mapper = req.app.get("mapper");
+    var etpStmts = req.app.get("stmt");
 
-    var stmt = mapper.getStatement('index', 'indexReqList', req.query.params, {language:'sql', indent: '  '});
-
+    // var options = {id:'admin'};
+    var options = {};
+    var stmt = etpStmts.IndexManage.selectIndexInfoOpenReqList(options);
     console.log(stmt);
 
     Promise.using(pool.connect(), conn => {
