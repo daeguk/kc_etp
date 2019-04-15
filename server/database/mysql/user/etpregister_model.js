81b8fe9d7469d64664c2f3e47e95e429b022a1e3
brutus3986 <brutus3986@example.com>
2019-04-15 19:18:08 +0900

    쿼리변경

--- server/database/mysql/user/etpregister_model.js
+++ server/database/mysql/user/etpregister_model.js
@@ -10,19 +10,23 @@ var model = {};
 model.selectEtpRegisterList = function(options) {
     console.log('selectEtpRegisterList');
 
-    var stmt = 'SELECT b.inst_name, a.isu_kor_nm, a.req_date, a.kor_for_type, \
-        ifnull(a.ridx_dist_sym_code, a.idx_sym_code) as basic_idx, \
-        a.isu_eng_nm \
+    var stmt = 'SELECT a.*, \
+        (select inst_nm from m001uetpinstcode where inst_cd = a.inst_cd limit 1) inst_nm, \
+        (select m_cd_nm from m001uetpcode where l_cd= "002" and m_cd = a.isin_stat_cd) m_cd_nm, \
+        timestampdiff(day,req_date,now()) lapse_date, \
+        timestampdiff(day,ifnull(list_date,"29991231")  ,now()) list_lapse_date, \
+        ifnull(a.ridx_dist_sym_code,a.idx_sym_code) basic_idx \
         from m001uetpmaster a \
         LEFT JOIN tm_domain_mast b \
         ON a.inst_cd = b.inst_cd \
-        WHERE 1=1';
+        WHERE (a.list_date    >=   now()  or      a.list_date is null) \
+        and a.isin_stat_cd != "0009" ';
 
     if (options.id != null && options != "") {
         stmt += ` AND ID = \'${options.id}\'`;
     }
 
-    stmt += ` ORDER BY a.req_date DESC`
+    stmt += `  order by  a.seq asc`
     return stmt;
 }
 
