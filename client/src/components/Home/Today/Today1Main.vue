077cda8b005b1bdf92a0c1c2d6bac415b796ee2f
yun yeon jung <82pring@aver.com>
2019-04-22 13:09:13 +0900

    디자인작업

--- client/src/components/Home/Today/Today1Main.vue
+++ client/src/components/Home/Today/Today1Main.vue
@@ -1,19 +1,48 @@
 <template>
     <v-layout row wrap>
         <v-flex xs12>
-            <h1> TODAY1 개발중....</h1>
+           <v-tabs
+                slot="extension"
+                v-model="tab"
+                align-with-title
+                light
+            >
+            <v-tabs-slider color="#35e0e2"></v-tabs-slider>
+    
+            <v-tab v-for="item in items" :key="item">
+                {{ item }}
+            </v-tab>
+            </v-tabs>
+
+            <v-tabs-items v-model="tab">
+                <v-tab-item>
+                    <indexmarkettab1></indexmarkettab1>
+                </v-tab-item>
+                <v-tab-item>
+                    <indexmarkettab2></indexmarkettab2>
+                </v-tab-item>
+        </v-tabs-items>
       </v-flex>
-    </v-layout> 
+    </v-layout>
 </template>
 
 <script>
 
+import indexmarkettab1   from  './indexmarkettab1.vue'
+import indexmarkettab2   from  './indexmarkettab2.vue'
+
 export default {
         data() {
             return {
+                tab: null,
+                drawer:"",
+                search:"",
+                items: ['TODAY', '지수사업자별'],
     	};
     },    
     components: {
+        indexmarkettab1     : indexmarkettab1,
+        indexmarkettab2     : indexmarkettab2,
     },
     created: function() {
     },
