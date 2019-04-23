daed8dc4b59bde295e8574253fc1cc146e13974f
chlqudrnr1 <chlqudrnr@gmail.com>
2019-04-23 15:53:13 +0900

    new 문구 추가

--- client/src/components/Home/MarketInfo/marketRealEstate.vue
+++ client/src/components/Home/MarketInfo/marketRealEstate.vue
@@ -367,8 +367,10 @@ export default {
                                             "render": function ( data, type, row ) {
                                                 let htm = "<span>";
                                                 htm += "           <b>"+data+"</b>";
-                                                htm += "            <br>"+row.f16013+" <span><div class='text_new'>new</div></span>";
-                                                htm += "        </span>";
+                                                htm += "            <br>"+row.f16013;
+                                                if (row.NEW_YN == "Y") {
+                                                    htm += "<span><div class='text_new'>new</div></span>";
+                                                }
                                                 return htm;
                                             },
                                             "targets": 0
