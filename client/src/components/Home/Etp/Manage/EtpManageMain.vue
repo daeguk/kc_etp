8b9823a05881996abbf4c6fdc18fd1ed1c5795bd
chlqudrnr1 <chlqudrnr@gmail.com>
2019-05-02 16:13:20 +0900

    ETP 운용관리

--- client/src/components/Home/Etp/Manage/EtpManageMain.vue
+++ client/src/components/Home/Etp/Manage/EtpManageMain.vue
@@ -1,17 +1,10 @@
 <template>
     <v-layout row wrap>
         <v-flex xs12>
-            <v-tabs
-                slot="extension"
-                v-model="tab"
-                align-with-title
-                light
-            >
-            <v-tabs-slider color="#1e99e8"></v-tabs-slider>
-    
-            <v-tab v-for="item in items" :key="item">
-                {{ item }}
-            </v-tab>
+            <v-tabs slot="extension" v-model="tab" align-with-title light>
+                <v-tabs-slider color="#1e99e8"></v-tabs-slider>
+
+                <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
             </v-tabs>
 
             <v-tabs-items v-model="tab">
@@ -24,28 +17,27 @@
                 <v-tab-item>
                     <Pdf></Pdf>
                 </v-tab-item>
-        </v-tabs-items>
-      </v-flex>
+            </v-tabs-items>
+        </v-flex>
     </v-layout>
 </template>
 
 <script>
- import Info   from  './Info.vue'
- import Index   from  './Index.vue'
- import Pdf   from  './Pdf.vue'
+import Info from "./Info.vue";
+import Index from "./Index.vue";
+import Pdf from "./Pdf.vue";
 
 export default {
-        data() {
-            return {
-                 tab: null,
-                 items: ['ETP 운용정보', '지수관리', 'PDF 관리'],
-    	};
-    },    
+    data() {
+        return {
+            tab: null,
+            items: ["ETP 운용정보", "지수관리", "PDF 관리"]
+        };
+    },
     components: {
-         Info     : Info,
-         Index     : Index,
-         Pdf     : Pdf,
+        Info: Info,
+        Index: Index,
+        Pdf: Pdf
     }
-    
-}
+};
 </script>
