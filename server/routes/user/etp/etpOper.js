ddd502b02bb079b2096730d4e8ac7dee4181be89
bkLove <chlqudrnr@gmail.com>
2019-05-08 21:39:38 +0900

    ETP 운용정보 수정

--- server/routes/user/etp/etpOper.js
+++ server/routes/user/etp/etpOper.js
@@ -15,7 +15,7 @@ var async = require('async');
 
 
 /*
- * ETP 운용정보를 조회한다.
+ * ETP 운용관리 - ETP 운영정보를 조회한다.
  * 2019-05-03  bkLove(촤병국)
  */
 var getEtpOperInfo = function(req, res) {
@@ -113,7 +113,7 @@ var getEtpOperInfo = function(req, res) {
 }
 
 /*
- * ETP 지수관리 정보를 조회한다.
+ * ETP 운용관리 - 지수관리 를 조회한다.
  * 2019-05-03  bkLove(촤병국)
  */
 var getEtpOperIndex = function(req, res) {
@@ -212,7 +212,204 @@ var getEtpOperIndex = function(req, res) {
 
 
 /*
- * ETP PDF 관리 정보를 조회한다.
+ * ETP 운용관리 - 지수관리 - 해외지수 종가 모니터링을 조회한다.
+ * 2019-05-03  bkLove(촤병국)
+ */
+var geEtpOperIndexOversea = function(req, res) {
+    try {
+        console.log('etpOper.geEtpOperIndexOversea 호출됨.');
+
+        var pool = req.app.get("pool");
+        var mapper = req.app.get("mapper");
+        var resultMsg = {};
+
+        /* 1. body.data 값이 있는지 체크 */
+        if (!req.body.data) {
+            console.log("[error] etpOper.geEtpOperIndexOversea  req.body.data no data.");
+            console.log(req.body.data);
+
+            resultMsg.result = false;
+            resultMsg.msg = "[error] etpOper.geEtpOperIndexOversea  req.body.data no data.";
+            
+            throw resultMsg;
+        }
+
+        var paramData = JSON.parse( JSON.stringify(req.body.data) );
+
+        paramData.user_id       =   req.session.user_id;
+        paramData.inst_cd       =   req.session.inst_cd;
+        paramData.type_cd       =   req.session.type_cd;
+        paramData.large_type    =   req.session.large_type;
+
+
+        var format = { language: 'sql', indent: '' };
+        var stmt = "";
+
+        Promise.using(pool.connect(), conn => {
+
+
+            async.waterfall([
+
+                /* 1. EtpBasic 의 기본정보를 조회한다. */
+                function( callback ) {
+
+                    stmt = mapper.getStatement('etpOper', 'geEtpOperIndexOversea', paramData, format);
+                    console.log(stmt);
+
+                    conn.query(stmt, function( err, rows ) {
+
+                        if( err ) {
+                            resultMsg.result    =   false;
+                            resultMsg.msg       =   "[error] etpOper.geEtpOperIndexOversea Error while performing Query";
+                            resultMsg.err       =   err;
+
+                            return callback( resultMsg );
+                        }
+
+                        if ( rows && rows.length > 0 ) {
+                            resultMsg.dataList  = rows;
+                        }
+
+                        callback( null );
+                    });
+                }
+
+            ], function (err) {
+
+                if( err ) {
+                    console.log( err );
+                }else{
+
+                    resultMsg.result    =   true;
+                    resultMsg.msg       =   "";
+                    resultMsg.err       =   null;
+                }
+
+                res.json( resultMsg );
+                res.end();
+            });
+        });
+
+    } catch(expetion) {
+
+        console.log(expetion);
+
+        if( resultMsg && !resultMsg.msg ) {
+            resultMsg.result    =   false;
+            resultMsg.msg       =   "[error] etpOper.geEtpOperIndexOversea 오류가 발생하였습니다.";
+            resultMsg.err       =   expetion;
+        }
+
+        resultMsg.dataList      =   [];
+
+        res.json({
+            resultMsg
+        });
+        res.end();  
+    }
+}
+
+/*
+ * ETP 운용관리 - 지수관리 - 오류내역을 조회한다.
+ * 2019-05-03  bkLove(촤병국)
+ */
+var geEtpOperIndexError = function(req, res) {
+    try {
+        console.log('etpOper.geEtpOperIndexError 호출됨.');
+
+        var pool = req.app.get("pool");
+        var mapper = req.app.get("mapper");
+        var resultMsg = {};
+
+        /* 1. body.data 값이 있는지 체크 */
+        if (!req.body.data) {
+            console.log("[error] etpOper.geEtpOperIndexError  req.body.data no data.");
+            console.log(req.body.data);
+
+            resultMsg.result = false;
+            resultMsg.msg = "[error] etpOper.geEtpOperIndexOversea  req.body.data no data.";
+            
+            throw resultMsg;
+        }
+
+        var paramData = JSON.parse( JSON.stringify(req.body.data) );
+
+        paramData.user_id       =   req.session.user_id;
+        paramData.inst_cd       =   req.session.inst_cd;
+        paramData.type_cd       =   req.session.type_cd;
+        paramData.large_type    =   req.session.large_type;
+
+
+        var format = { language: 'sql', indent: '' };
+        var stmt = "";
+
+        Promise.using(pool.connect(), conn => {
+
+
+            async.waterfall([
+
+                /* 1. EtpBasic 의 기본정보를 조회한다. */
+                function( callback ) {
+
+                    stmt = mapper.getStatement('etpOper', 'geEtpOperIndexError', paramData, format);
+                    console.log(stmt);
+
+                    conn.query(stmt, function( err, rows ) {
+
+                        if( err ) {
+                            resultMsg.result    =   false;
+                            resultMsg.msg       =   "[error] etpOper.geEtpOperIndexError Error while performing Query";
+                            resultMsg.err       =   err;
+
+                            return callback( resultMsg );
+                        }
+
+                        if ( rows && rows.length > 0 ) {
+                            resultMsg.dataList  = rows;
+                        }
+
+                        callback( null );
+                    });
+                }
+
+            ], function (err) {
+
+                if( err ) {
+                    console.log( err );
+                }else{
+
+                    resultMsg.result    =   true;
+                    resultMsg.msg       =   "";
+                    resultMsg.err       =   null;
+                }
+
+                res.json( resultMsg );
+                res.end();
+            });
+        });
+
+    } catch(expetion) {
+
+        console.log(expetion);
+
+        if( resultMsg && !resultMsg.msg ) {
+            resultMsg.result    =   false;
+            resultMsg.msg       =   "[error] etpOper.geEtpOperIndexError 오류가 발생하였습니다.";
+            resultMsg.err       =   expetion;
+        }
+
+        resultMsg.dataList      =   [];
+
+        res.json({
+            resultMsg
+        });
+        res.end();  
+    }
+}
+
+
+/*
+ * ETP 운용관리 - PDF관리 정보를 조회한다.
  * 2019-05-03  bkLove(촤병국)
  */
 var getEtpOperPdf = function(req, res) {
@@ -312,5 +509,7 @@ var getEtpOperPdf = function(req, res) {
 
 module.exports.getEtpOperInfo = getEtpOperInfo;
 module.exports.getEtpOperIndex = getEtpOperIndex;
+module.exports.geEtpOperIndexOversea = geEtpOperIndexOversea;
+module.exports.geEtpOperIndexError = geEtpOperIndexError;
 module.exports.getEtpOperPdf = getEtpOperPdf;
 
