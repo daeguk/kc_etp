e92b344495a613f643b8244ceb53114b72a9c1ec
daeguk <daeguk@naver.com>
2019-05-16 10:21:44 +0900

    변수 수정

--- client/src/components/Home/Index/Manage/IndexDetailInfoTab2.vue
+++ client/src/components/Home/Index/Manage/IndexDetailInfoTab2.vue
@@ -218,112 +218,113 @@ export default {
     methods: {
         init: function() {
             var vm = this;
+            vm.$nextTick().then(() => {
+                if(     vm.basicData 
+                    &&  vm.basicData.jisu_cd
+                    &&  vm.basicData.large_type
+                    &&  vm.basicData.market_id
+                ) {
+                    vm.param.jisu_cd      =   vm.basicData.jisu_cd;
+                    vm.param.large_type   =   vm.basicData.large_type;
+                    vm.param.market_id    =   vm.basicData.market_id;
+                    vm.perf_class = vm.basicData.perf_class;
+                    vm.tbl_class = vm.basicData.tbl_class;
+                    vm.chart_size = vm.basicData.chart_size;
+                }
+                else if(   
+                        vm.$route.query.jisu_cd  
+                    &&  vm.$route.query.large_type  
+                    &&  vm.$route.query.market_id  
+                ) {
+                    vm.param.jisu_cd      =   vm.$route.query.jisu_cd;
+                    vm.param.large_type   =   vm.$route.query.large_type;
+                    vm.param.market_id    =   vm.$route.query.market_id;
+                }        
 
-            if(     vm.basicData 
-                &&  vm.basicData.jisu_cd
-                &&  vm.basicData.large_type
-                &&  vm.basicData.market_id
-            ) {
-                vm.param.jisu_cd      =   vm.basicData.jisu_cd;
-                vm.param.large_type   =   vm.basicData.large_type;
-                vm.param.market_id    =   vm.basicData.market_id;
-                vm.perf_class = vm.basicData.perf_class;
-                vm.tbl_class = vm.basicData.tbl_class;
-                vm.chart_size = vm.basicData.chart_size;
-            }
-            else if(   
-                    vm.$route.query.jisu_cd  
-                &&  vm.$route.query.large_type  
-                &&  vm.$route.query.market_id  
-            ) {
-                vm.param.jisu_cd      =   vm.$route.query.jisu_cd;
-                vm.param.large_type   =   vm.$route.query.large_type;
-                vm.param.market_id    =   vm.$route.query.market_id;
-            }        
 
+                vm.$root.$infopoptab1 = vm.$refs.$infopoptab1;
 
-            vm.$root.$infopoptab1 = vm.$refs.$infopoptab1;
-
-            if(perf_table) {
-                perf_table.destroy();
-            }
-            perf_table = $('#perf_table').DataTable( {
-                "processing": true,
-                "serverSide": false,
-                "search": true,
-                "info": false,   // control table information display field
-                "stateSave": true,  //restore table state on page reload,
-                "lengthMenu": [[10, 20, 50, -1], [10, 20, 50, "All"]],
-                select: {
-                    style:    'single',
-                    selector: 'td:first-child'
-                },
-                paging: false,
-                searching: false,
-                "ordering": false,
-                "columnDefs": [
-                    {  
-                        "render": function ( data, type, row, meta ) {
-                            if (data) {
-                                return "<img src='/assets/img/" + vm.performChartImages[ meta.row ] + "'><span>&nbsp;&nbsp;&nbsp;" + data + "</span>";
-                            } else {
-                                return "";
-                            }
-                        },
-                        "targets": 1
+                if(perf_table) {
+                    perf_table.destroy();
+                }
+                perf_table = $('#perf_table').DataTable( {
+                    "processing": true,
+                    "serverSide": false,
+                    "search": true,
+                    "info": false,   // control table information display field
+                    "stateSave": true,  //restore table state on page reload,
+                    "lengthMenu": [[10, 20, 50, -1], [10, 20, 50, "All"]],
+                    select: {
+                        style:    'single',
+                        selector: 'td:first-child'
                     },
-                    {
-                        "targets": [ 0 ],
-                        "visible": false
-                    },
-                    {  
-                        "render": function ( data, type, row ) {
-                            if (data) {
-                                // 기본 지수는 삭제 버튼 제외
-                                if (row.F16013 != vm.$route.query.jisu_cd) {
-                                    return "<div class='tooltip'><button type='button' id='per_del' class='btn_icon v-icon material-icons'>delete</button><span class='tooltiptext' style='width:40px;'>삭제</span></div>";
-                                } 
-                            } else {
-                                return "";
-                            }
+                    paging: false,
+                    searching: false,
+                    "ordering": false,
+                    "columnDefs": [
+                        {  
+                            "render": function ( data, type, row, meta ) {
+                                if (data) {
+                                    return "<img src='/assets/img/" + vm.performChartImages[ meta.row ] + "'><span>&nbsp;&nbsp;&nbsp;" + data + "</span>";
+                                } else {
+                                    return "";
+                                }
+                            },
+                            "targets": 1
                         },
-                        "targets": 10
-                    },                
-                ],
-                data : [],
-                columns: [
-                    { "data": "F16013", "orderable": false}, 
-                    { "data": "F16002", "orderable": false,  "width":"30%", className: 'txt_left line2'}, 
-                    { "data": "Week1", "orderable": false, className: 'txt_right'},
-                    { "data": "Month1", "orderable": false, className: 'txt_right'},
-                    { "data": "Month3", "orderable": false, className: 'txt_right'},
-                    { "data": "YTD", "orderable": false, className: 'txt_right'},
-                    { "data": "Year1", "orderable": false, className: 'txt_right'},
-                    { "data": "Year3", "orderable": false, className: 'txt_right'},
-                    { "data": "Year5", "orderable": false, className: 'txt_right'},
-                    { "data": "Year10", "orderable": false, className: 'txt_right'},
-                    {"data": null, "align":"center", className: '', defaultContent:""}
-                ]
-            }); 
-        
+                        {
+                            "targets": [ 0 ],
+                            "visible": false
+                        },
+                        {  
+                            "render": function ( data, type, row ) {
+                                if (data) {
+                                    // 기본 지수는 삭제 버튼 제외
+                                    if (row.F16013 != vm.$route.query.jisu_cd) {
+                                        return "<div class='tooltip'><button type='button' id='per_del' class='btn_icon v-icon material-icons'>delete</button><span class='tooltiptext' style='width:40px;'>삭제</span></div>";
+                                    } 
+                                } else {
+                                    return "";
+                                }
+                            },
+                            "targets": 10
+                        },                
+                    ],
+                    data : [],
+                    columns: [
+                        { "data": "F16013", "orderable": false}, 
+                        { "data": "F16002", "orderable": false,  "width":"30%", className: 'txt_left line2'}, 
+                        { "data": "Week1", "orderable": false, className: 'txt_right'},
+                        { "data": "Month1", "orderable": false, className: 'txt_right'},
+                        { "data": "Month3", "orderable": false, className: 'txt_right'},
+                        { "data": "YTD", "orderable": false, className: 'txt_right'},
+                        { "data": "Year1", "orderable": false, className: 'txt_right'},
+                        { "data": "Year3", "orderable": false, className: 'txt_right'},
+                        { "data": "Year5", "orderable": false, className: 'txt_right'},
+                        { "data": "Year10", "orderable": false, className: 'txt_right'},
+                        {"data": null, "align":"center", className: '', defaultContent:""}
+                    ]
+                }); 
+            
 
-            if(     this.param
-                &&  this.param.jisu_cd
-                &&  this.param.market_id
-            ) {
+                if(     this.param
+                    &&  this.param.jisu_cd
+                    &&  this.param.market_id
+                ) {
 
-                vm.getIndexImportanceList( this.param );
+                    vm.getIndexImportanceList( this.param );
 
-                $('#perf_table, tbody').on('click', 'button', function () {
-                    if ($(this).attr('id') == 'per_del') {
-                        var data = perf_table.row($(this).parents('tr')).remove().draw();
-                    
-                        vm.performance_chart();
-                    }
-                });
+                    $('#perf_table, tbody').on('click', 'button', function () {
+                        if ($(this).attr('id') == 'per_del') {
+                            var data = perf_table.row($(this).parents('tr')).remove().draw();
+                        
+                            vm.performance_chart();
+                        }
+                    });
 
-                vm.getIndexAnalysisInfo();
-            }
+                    vm.getIndexAnalysisInfo();
+                }
+            });
         },
         performance_chart: function() {
             var vm = this;
