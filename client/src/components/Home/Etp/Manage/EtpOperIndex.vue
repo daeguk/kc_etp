869f6c977567ab48a09febfd4f88636d5f98cae6
bkLove <chlqudrnr@gmail.com>
2019-06-03 15:17:03 +0900

    ETP 수정

--- client/src/components/Home/Etp/Manage/EtpOperIndex.vue
+++ client/src/components/Home/Etp/Manage/EtpOperIndex.vue
@@ -168,7 +168,7 @@ export default {
                 "info": false,   // control table information display field
                 "stateSave": true,  //restore table state on page reload,
                 "lengthMenu": [[10, 20, 50, -1], [10, 20, 50, "All"]],
-                "scrollY": '70vh',         
+                "scrollY": '75vh',         
                 paging: false,
                 searching: false,
                 data : [],
--- client/src/components/Home/Etp/Manage/EtpOperInfo.vue
+++ client/src/components/Home/Etp/Manage/EtpOperInfo.vue
@@ -162,7 +162,7 @@ export default {
                 "info": false,   // control table information display field
                 "stateSave": true,  //restore table state on page reload,
                 "lengthMenu": [[10, 20, 50, -1], [10, 20, 50, "All"]],
-                "scrollY": '70vh',
+                "scrollY": '73vh',
                 paging: false,
                 searching: false,
                 data : [],
@@ -351,7 +351,7 @@ export default {
                     "info": false,   // control table information display field
                     "stateSave": true,  //restore table state on page reload,
                     "lengthMenu": [[10, 20, 50, -1], [10, 20, 50, "All"]],
-                    "scrollY": '70vh',
+                    "scrollY": '75vh',
                     paging: false,
                     searching: false,
                     data : [],
