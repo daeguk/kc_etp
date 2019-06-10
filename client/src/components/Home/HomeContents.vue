79f60179205ffae9933c34f09c4349b90eced9ff
daeguk <daeguk@naver.com>
2019-06-10 17:20:28 +0900

    사업자별 메인페이지 수정

--- client/src/components/Home/HomeContents.vue
+++ client/src/components/Home/HomeContents.vue
@@ -9,7 +9,7 @@
 </template> 
 
 <script>
-
+import Config       from "@/js/config.js";
 export default {
     data() {
         return {
@@ -20,7 +20,19 @@ export default {
     beforeCreate() {
     },
     created: function() {
-        this.$router.push({ path: '/info/etpinfo'});
+        /* 지수 사업자 */
+        if (this.$store.state.user.type_cd == '0003') {
+            this.$router.push({ path: Config.jisu_url });
+        /* ETP 발행사 */
+        } else if (this.$store.state.user.type_cd == '0001' 
+            || this.$store.state.user.type_cd == '0002' 
+            || this.$store.state.user.type_cd == '0004'  
+            || this.$store.state.user.type_cd == '9998'  
+            || this.$store.state.user.type_cd == '9999') {
+            this.$router.push({ path: Config.etp_url });
+        } else {
+            this.$router.push({ path: Config.home_url });
+        }
     },
     beforeDestroy() {
     },
