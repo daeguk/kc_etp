e9166324106cf7d05b8ca1686b1e9153226b0f82
daeguk <daeguk@naver.com>
2019-06-20 10:21:58 +0900

    단위 vh에서 px로 변경

--- client/src/components/common/control/ComFavorItemSub.vue
+++ client/src/components/common/control/ComFavorItemSub.vue
@@ -265,7 +265,7 @@ export default {
             "info": false,   // control table information display field
             "stateSave": true,  //restore table state on page reload,
             "lengthMenu": [[10, 20, 50, -1], [10, 20, 50, "All"]],
-            "scrollY":        '125vh',
+            "scrollY":        '500px',
             thead: {
                 display:'none'
             },
