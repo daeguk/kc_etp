6039a6b81e7a93a75d6bd3a053164701b29dd104
leejs814 <leejs814@nate.com>
2019-06-20 11:09:48 +0900

    오타수정

--- client/src/components/Home/Etp/Register/EtpApplyList.vue
+++ client/src/components/Home/Etp/Register/EtpApplyList.vue
@@ -5,7 +5,7 @@
                 <v-card flat>
                     <v-card-title primary-title>
                         <h3 class="headline mb-0">
-                            EPT 신청현황 |
+                            ETP 신청현황 |
                             <span
                                 class="grey--text"
                             >코스콤에 신청하신 ETP목록 및 ETP별 진행 상태를 확인 할 수 있습니다.</span>
