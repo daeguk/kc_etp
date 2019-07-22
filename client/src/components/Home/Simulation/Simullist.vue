c075deae204c81c2040a07c515607827595fed0a
yun yeon jung <82pring@aver.com>
2019-07-22 16:38:56 +0900

    디자인 수정

--- client/src/components/Home/Simulation/Simullist.vue
+++ client/src/components/Home/Simulation/Simullist.vue
@@ -8,7 +8,57 @@
              <v-tab-item>
                 <v-layout row wrap class="content_margin">
                     <v-flex grow>
-                        <v-card flat>시뮬리스트</v-card>
+                        <v-card flat class="creat_f">
+                            <span><v-btn depressed color="primary">Create Senario</v-btn></span>
+                                <span><v-btn depressed outline color="primary">Create A Group</v-btn></span>
+                                <span><v-text-field  label="unnamed-1" outline class="wid200"></v-text-field></span>
+                                <span class="margin_t1"><v-btn depressed small outline color="primary">추가</v-btn></span>
+                        </v-card>
+                        <v-card flat>
+                            <table class="tbl_type ver10">
+                            <caption> </caption>
+                            <colgroup>
+                                <col width="40%">
+                                <col width="20%">
+                                <col width="20%">
+                                <col width="20%">
+                            </colgroup>
+                            <thead>
+                            <tr>
+                                <th class="txt_left">Name</th>
+                                <th class="txt_right">Index</th>
+                                <th>Last modifired</th>
+                                <th class="txt_right"></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="txt_left"><v-icon>folder_open</v-icon> KIMS Kosdaq TOP50 Index</td>
+                                <td class="txt_right"></td>
+                                <td></td>
+                                <td class="txt_right"></td>
+                            </tr>
+                            <tr>
+                                <td class="txt_left"><div class="folder_2dep"><v-icon>description</v-icon> KIMS Kosdaq TOP50 Senario 1</div></td>
+                                <td class="txt_right">2,076.93<br><span class="text_S text_blue">0.47%</span></td>
+                                <td>2019.01.12</td>
+                                <td class="txt_right">
+                                    <button class='btn_icon v-icon material-icons' @click="">inbox</button>
+                                    <button class='btn_icon v-icon material-icons' @click="">equalizer</button>
+                                    <button class='btn_icon v-icon material-icons' @click="">more_horiz</button></td>
+                            </tr>
+                            <tr>
+                                <td class="txt_left"><div class="folder_2dep"><v-icon>description</v-icon> KIMS Kosdaq TOP50 Senario 1</div></td>
+                                <td class="txt_right">2,076.93<br><span class="text_S text_red">-0.47%</span></td>
+                                <td>2019.01.12</td>
+                                <td class="txt_right">
+                                    <button class='btn_icon v-icon material-icons' @click="">inbox</button>
+                                    <button class='btn_icon v-icon material-icons' @click="">equalizer</button>
+                                    <button class='btn_icon v-icon material-icons' @click="">more_horiz</button></td>
+                            </tr>
+                        </tbody>
+                        </table>
+                        </v-card>
                     </v-flex>
                 </v-layout>
 
