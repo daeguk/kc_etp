959543d91e8fb09dcb24682631abbf480687606a
bkLove <chlqudrnr@gmail.com>
2019-08-21 15:16:16 +0900

    소수점 둘째자리까지 노출되게 수정

--- client/src/components/Home/Simulation/SimulationResult.vue
+++ client/src/components/Home/Simulation/SimulationResult.vue
@@ -301,13 +301,13 @@ export default {
                                     );                                                                                                              /* 종목 */
 
                                     /* 변경전 */
-                                    sub_item.fmt_BEFORE_IMPORTANCE      =   (
+                                    sub_item.fmt_BEFORE_IMPORTANCE      =   util.formatNumber(
                                         !sub_item.BEFORE_IMPORTANCE ?   
                                         0 : ( Math.round( sub_item.BEFORE_IMPORTANCE * 100 ) / 100 ) 
                                     ) + " %";
 
                                     /* 변경후 */
-                                    sub_item.fmt_AFTER_IMPORTANCE       =   (
+                                    sub_item.fmt_AFTER_IMPORTANCE       =   util.formatNumber(
                                         !sub_item.AFTER_IMPORTANCE ?
                                         0 : ( Math.round( sub_item.AFTER_IMPORTANCE * 100 ) / 100 )
                                     ) + " %";
@@ -343,11 +343,13 @@ export default {
                     **************************************************************************************************************/
                         vm.paramData.arr_daily.forEach( function( item, index, array ) {
                             item.fmt_F12506             =   util.formatDate( new String( item.F12506 ) );                                       /* 일자 */
-                            item.fmt_INDEX_RATE         =   Math.round( item.INDEX_RATE * 100 ) / 100;                                          /* Index */
-                            item.fmt_balance            =   ( 
+                            item.fmt_INDEX_RATE         =   util.formatNumber(
+                                Math.round( item.INDEX_RATE * 100 ) / 100
+                        );                                                                                                                      /* Index */
+                            item.fmt_balance            =   util.formatNumber( 
                                 Math.round( ( vm.simul_result_mast.init_invest_money * item.RETURN_VAL ) * 100 ) / 100 
                             );                                                                                                                  /* balance = 초기투자금액 * return_val */
-                            item.fmt_RETURN_VAL         =   (
+                            item.fmt_RETURN_VAL         =   util.formatNumber(
                                 Math.round(
                                     (
                                         Math.round( item.RETURN_VAL * 10000 ) / 10000
@@ -436,13 +438,13 @@ export default {
                                     );                                                                                                              /* 종목 */
 
                                     /* 변경전 */
-                                    sub_item.fmt_BEFORE_IMPORTANCE      =   (
+                                    sub_item.fmt_BEFORE_IMPORTANCE      =   util.formatNumber(
                                         !sub_item.BEFORE_IMPORTANCE ?   
                                         0 : ( Math.round( sub_item.BEFORE_IMPORTANCE * 100 ) / 100 ) 
                                     ) + " %";
 
                                     /* 변경후 */
-                                    sub_item.fmt_AFTER_IMPORTANCE       =   (
+                                    sub_item.fmt_AFTER_IMPORTANCE       =   util.formatNumber(
                                         !sub_item.AFTER_IMPORTANCE ?
                                         0 : ( Math.round( sub_item.AFTER_IMPORTANCE * 100 ) / 100 )
                                     ) + " %";
@@ -478,11 +480,13 @@ export default {
                         **************************************************************************************************************/
                             response.data.arr_result_daily.forEach( function( item, index, array ) {
                                 item.fmt_F12506             =   util.formatDate( new String( item.F12506 ) );                                       /* 일자 */
-                                item.fmt_INDEX_RATE         =   Math.round( item.INDEX_RATE * 100 ) / 100;                                          /* Index */
-                                item.fmt_balance            =   (
+                                item.fmt_INDEX_RATE         =   util.formatNumber(
+                                    Math.round( item.INDEX_RATE * 100 ) / 100
+                                );                                                                                                                  /* Index */
+                                item.fmt_balance            =   util.formatNumber(
                                     Math.round( ( vm.simul_result_mast.init_invest_money * item.RETURN_VAL ) * 100 ) / 100 
                                 );                                                                                                                  /* balance = 초기투자금액 * return_val */
-                                item.fmt_RETURN_VAL         =   (
+                                item.fmt_RETURN_VAL         =   util.formatNumber(
                                     Math.round(
                                         (
                                             Math.round( item.RETURN_VAL * 10000 ) / 10000
