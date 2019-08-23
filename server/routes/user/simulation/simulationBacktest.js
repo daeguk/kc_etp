84934cc8d0c0fbfd4636974eeea0d943ad9d473e
bkLove <chlqudrnr@gmail.com>
2019-08-23 18:26:57 +0900

    시뮬레이션 수정

--- server/routes/user/simulation/simulationBacktest.js
+++ server/routes/user/simulation/simulationBacktest.js
@@ -3537,6 +3537,9 @@ var fn_get_simulation_data  =   function(
                                    
                             
                                     
+if( ["20180702"].includes( p_param.F12506 ) ) {
+    console.log( "p_param.F12506", p_param.F12506, "v_dataKey", v_dataKey, "F15028", F15028, "F16143", v_dataItem.F16143, "TODAY_RATE", v_dataItem.TODAY_RATE, "tot_F15001", p_dailyObj[ p_param.v_before_F12506 ].tot_F15001, "\n\n" );
+}                                    
                             
                                 
                         }
@@ -3751,7 +3754,6 @@ var fn_get_simulation_data  =   function(
             );  
 
             Object.assign( p_dailyObj[ p_param.F12506 ], totalInfo );
-
 
 
             /* 최초 영업일이 아닌 경우 */
