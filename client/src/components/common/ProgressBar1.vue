31c033e43c18e1a0cea1daa1cb05f79dc5078d20
yun yeon jung <82pring@aver.com>
2019-11-07 14:07:29 +0900

    디자인 추가

---
+++ client/src/components/common/ProgressBar1.vue
@@ -0,0 +1,52 @@
+<template>
+<v-dialog v-model="dialog" persistent content-class="progressbar_w1">     
+  <template>
+      <v-card flat class="progress_1">
+   <v-row
+        class="fill-height"
+        align-content="center"
+        justify="center"
+      >
+        <v-col
+          class="subtitle-1 text-center"
+          cols="6"
+        >
+          몇분만 기다려 주세요
+        </v-col>
+        <v-col cols="10">
+          <v-progress-linear
+            color="primary accent-4"
+            indeterminate
+            rounded
+            height="6"
+          ></v-progress-linear>
+        </v-col>
+      </v-row>
+      </v-card>
+  </template>
+</v-dialog>
+</template>
+<script>
+export default {
+  props: [],
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  components: {
+  },
+  computed: {},
+  created: function() {},
+  beforeDestroy() {},
+  mounted: function() {},
+  methods: {
+    open() {
+      this.dialog = true;
+    },
+    close() {
+      this.dialog = false;
+    }
+  }
+};
+</script>
