55c0e4f73973bf2767799dffab2ab251e124bb55
yun yeon jung <82pring@aver.com>
2019-11-11 14:45:04 +0900

    디자인 수정

--- client/src/components/common/WaitProgressBar.vue
+++ client/src/components/common/WaitProgressBar.vue
@@ -2,31 +2,29 @@
 <v-dialog v-model="dialog" persistent content-class="progressbar_w1">     
   <template>
       <v-card flat class="progress_1">
-   <v-row
+   <v-layout
         class="fill-height"
         align-content="center"
         justify="center"
       >
-        <v-col
-          class="subtitle-1 text-center"
-          cols="6"
-        >
-        <v-flex class="pogress_1icon">
-            <v-icon>error</v-icon>
+        <v-flex class="subtitle-1 text-center">
+        <v-card flat>
+            <v-flex class="pogress_1icon">
+                <v-icon>error</v-icon>
+             </v-flex>
+        <v-flex>Please wait a minute<br>잠시만 기다려주세요</v-flex>
+        </v-card>
         </v-flex>
-        <v-flex>
-            Please wait a minute<br>잠시만 기다려주세요
-        </v-flex>
-        </v-col>
-        <v-col cols="10">
+      </v-layout>
+      <v-card flat>
           <v-progress-linear
             color="#1e99e8"
             indeterminate
             rounded
             height="6"
           ></v-progress-linear>
-        </v-col>
-      </v-row>
+    
+      </v-card>
       </v-card>
   </template>
 </v-dialog>
