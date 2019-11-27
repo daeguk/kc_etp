1b2937aae22437af593757f1af72c15a0b803ac6
daeguk <daeguk@naver.com>
2019-11-27 15:14:32 +0900

    시작비중 값 변경

--- server/routes/user/simulation/simulationBacktest.js
+++ server/routes/user/simulation/simulationBacktest.js
@@ -4257,7 +4257,7 @@ var	fn_get_simulation_data  =   function(
 
                                 var jongmokKey  =   Object.keys(contributeItem.jongmok)[jongmokIndex];
 
-                                contributeItem.jongmok[jongmokKey].START_WEIGHT = v_dailyJongmokObj[contributeItem.start_date][jongmokKey].TODAY_IMPORTANCE;
+                                contributeItem.jongmok[jongmokKey].START_WEIGHT = v_dailyJongmokObj[contributeItem.start_date][jongmokKey].AFTER_IMPORTANCE;
                                 contributeItem.jongmok[jongmokKey].END_WEIGHT = v_dailyJongmokObj[contributeItem.end_date][jongmokKey].TODAY_IMPORTANCE;
 
 
