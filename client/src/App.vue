8990d4a56c884234df6d47e88c5bc2eea9f0b4fb
threeon <ibksyong@gmail.com>
2019-12-05 17:14:15 +0900

    배포

--- client/src/App.vue
+++ client/src/App.vue
@@ -35,7 +35,7 @@ export default {
     console.log(navigator.platform);
     if ( navigator.platform ) { 
       if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
-        // this.$router.push({ path: '/mobile'});
+        this.$router.push({ path: '/mobile'});
       } else { 
         this.$router.push({ path: '/'});
         // this.$router.push({ path: '/mobile'});
