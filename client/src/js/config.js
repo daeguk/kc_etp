2dbae88b65ae1339e97304257a0e50c563c2ba47
threeon <ibksyong@gmail.com>
2019-12-10 17:34:49 +0900

    config 수정

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -3,12 +3,12 @@
  */
 
 module.exports = {
-  // base_url: "http://127.0.0.1:8034",
+  base_url: "http://127.0.0.1:8034",
   // base_url: "http://emptest.koscom.co.kr",
-  base_url: "https://emp.koscom.co.kr",
-  // ws_url: "ws://127.0.0.1:8034",
+  // base_url: "https://emp.koscom.co.kr",
+  ws_url: "ws://127.0.0.1:8034",
   // ws_url: "ws://emptest.koscom.co.kr",
-  ws_url: "wss://emp.koscom.co.kr",
+  // ws_url: "wss://emp.koscom.co.kr",
   home_url: "/info/etpinfo/",
   etp_url: "/etp/manage",
   jisu_url: "/index/manage/",
