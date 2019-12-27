c1d57ed015d426296265da2e98ad4c169e201ed6
bkLove <chlqudrnr@gmail.com>
2019-12-27 10:18:28 +0900

    sitem, eitem, citem 의 FLOW 값이 undefined 이면 해당값 자체를 노출하지 않도록 수정

--- client/src/components/common/chart/FundFlowChart.vue
+++ client/src/components/common/chart/FundFlowChart.vue
@@ -33,51 +33,60 @@ export default {
   },
   methods: {
     drawChart: function() {
-      var c = this.ctx;
-      var rad = 30;
-      var cX1 = 60;
-      var cX2 = 460;
-      var clen = cX2 - cX1;
-      var rlen = cX2 - cX1 - (rad * 4);
-      var cX = cX1 + (rad * 2) + rlen * ((this.num - this.rank) / this.num);
 
-      // console.log("rank : " + this.rank + " num : " + this.num);
-      // console.log("cX : " + cX);
-      var cY = 100;
+      if(   typeof this.sitem != "undefined" 
+        &&  typeof this.eitem != "undefined"
+        &&  typeof this.citem != "undefined"
 
-      c.clearRect(0, 0, this.chart.width, this.chart.height);
-      c.textAlign = 'center';
+        &&  typeof this.sitem.FLOW != "undefined" 
+        &&  typeof this.eitem.FLOW != "undefined" 
+        &&  typeof this.citem.FLOW != "undefined" 
+      ) {
+        var c = this.ctx;
+        var rad = 30;
+        var cX1 = 60;
+        var cX2 = 460;
+        var clen = cX2 - cX1;
+        var rlen = cX2 - cX1 - (rad * 4);
+        var cX = cX1 + (rad * 2) + rlen * ((this.num - this.rank) / this.num);
 
-      c.beginPath();
-      c.arc(cX1, cY, rad, 0, Math.PI*2);
-      c.fillStyle = "#CFD8DC";
-      c.fill(); 
-      c.closePath();
+        // console.log("rank : " + this.rank + " num : " + this.num);
+        // console.log("cX : " + cX);
+        var cY = 100;
 
-      c.beginPath();
-      c.arc(cX2, cY, rad, 0, Math.PI*2);
-      c.fill(); 
-      c.closePath();
+        c.clearRect(0, 0, this.chart.width, this.chart.height);
+        c.textAlign = 'center';
 
-      c.fillRect(cX1, cY-5, clen, 10);
+        c.beginPath();
+        c.arc(cX1, cY, rad, 0, Math.PI*2);
+        c.fillStyle = "#CFD8DC";
+        c.fill(); 
+        c.closePath();
 
-      c.fillStyle = "#424242";
-      c.font = '14px san-serif';
-      c.fillText(Math.round( ( ( typeof this.sitem == "undefined" || typeof this.sitem.FLOW == "undefined" ) ? 0 : this.sitem.FLOW )/100000000), cX1, cY+5);
-      c.fillText(this.sitem.F16002, cX1, cY+55);
-      c.fillText(Math.round( ( ( typeof this.eitem == "undefined" || typeof this.eitem.FLOW == "undefined" ) ? 0 : this.eitem.FLOW )/100000000), cX2, cY+5);
-      c.fillText(this.eitem.F16002, cX2, cY+55);
+        c.beginPath();
+        c.arc(cX2, cY, rad, 0, Math.PI*2);
+        c.fill(); 
+        c.closePath();
 
-      c.beginPath();
-      c.arc(cX, cY, rad, 0, Math.PI*2);
-      c.fillStyle = "#85c406";
-      c.fill(); 
-      c.closePath();
-      c.fillStyle = "#FFFFFF";
-      c.fillText(Math.round(this.citem.FLOW/100000000), cX, cY+5);
-      c.fillStyle = "#424242";
-      c.fillText("RANK (" + this.rank + " / " + this.num + ")", cX, cY-40);
+        c.fillRect(cX1, cY-5, clen, 10);
 
+        c.fillStyle = "#424242";
+        c.font = '14px san-serif';
+        c.fillText(Math.round(this.sitem.FLOW/100000000), cX1, cY+5);
+        c.fillText(this.sitem.F16002, cX1, cY+55);
+        c.fillText(Math.round(this.eitem.FLOW/100000000), cX2, cY+5);
+        c.fillText(this.eitem.F16002, cX2, cY+55);
+
+        c.beginPath();
+        c.arc(cX, cY, rad, 0, Math.PI*2);
+        c.fillStyle = "#85c406";
+        c.fill(); 
+        c.closePath();
+        c.fillStyle = "#FFFFFF";
+        c.fillText(Math.round(this.citem.FLOW/100000000), cX, cY+5);
+        c.fillStyle = "#424242";
+        c.fillText("RANK (" + this.rank + " / " + this.num + ")", cX, cY-40);
+      }
 
     },
   }
