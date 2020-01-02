9da72adebc4db78dbe6c2ecd51f195b2be306ffb
bkLove <chlqudrnr@gmail.com>
2020-01-02 14:14:49 +0900

    [routes/sample] js 소스 팩킹

--- server/routes/sample/fileupload.js
+++ server/routes/sample/fileupload.js
@@ -1,104 +1,72 @@
-
 var multer = require('multer');
-
 /* logging 추가함.  2019-06-10 */
 var log = require('../../util/logg');
 
-
-
-var fileuploadSingle = function (req, res) {
-
-    //log.debug("body ===", req);
-
-    //var deferred = Q.defer();
-    var storage = multer.diskStorage({
-        // 서버에 저장할 폴더
-        destination: function (req, file, cb) {
-            cb(null, "d:\\test");
-        },
-
-        // 서버에 저장할 파일 명
-        filename: function (req, file, cb) {
-
-            log.debug("file" + JSON.stringify(file));
-
-            file.uploadedFile = {
-                name: file.originalname
-            };
-
-            log.debug("filename=" + file.uploadedFile.name);
-            cb(null, file.uploadedFile.name);
-        }
-    });
-
-
-
-    var upload = multer({ storage: storage }).single('files');
-
-    upload(req, res, function (err) {
-        if (err) {
-            log.debug("File Upload Err" + err);
-        }
-        /*if (err) deferred.reject();
-        else deferred.resolve(req.file.uploadedFile);
-        */
-
-        //log.debug(req.file.uploadedFile);
-
-        res.end();
-    });
-
-    //return deferred.promise;
+var fileuploadSingle = function(req, res) {
+  //log.debug("body ===", req);
+  //var deferred = Q.defer();
+  var storage = multer.diskStorage({
+    // 서버에 저장할 폴더
+    destination: function(req, file, cb) {
+      cb(null, "d:\\test");
+    },
+    // 서버에 저장할 파일 명
+    filename: function(req, file, cb) {
+      log.debug("file" + JSON.stringify(file));
+      file.uploadedFile = {
+        name: file.originalname
+      };
+      log.debug("filename=" + file.uploadedFile.name);
+      cb(null, file.uploadedFile.name);
+    }
+  });
+  var upload = multer({
+    storage: storage
+  }).single('files');
+  upload(req, res, function(err) {
+    if(err) {
+      log.debug("File Upload Err" + err);
+    }
+    /*if (err) deferred.reject();
+    else deferred.resolve(req.file.uploadedFile);
+    */
+    //log.debug(req.file.uploadedFile);
+    res.end();
+  });
+  //return deferred.promise;
 };
-
-
-
-var fileuploadMulti = function (req, res) {
-
-    //log.debug("body ===", req);
-
-    //var deferred = Q.defer();
-    var storage = multer.diskStorage({
-        // 서버에 저장할 폴더
-        destination: function (req, file, cb) {
-            cb(null, "d:\\test");
-        },
-
-        // 서버에 저장할 파일 명
-        filename: function (req, file, cb) {
-
-            log.debug("file" + JSON.stringify(file));
-
-            file.uploadedFile = {
-                name: file.originalname
-            };
-
-            log.debug("filename=" + file.uploadedFile.name);
-            cb(null, file.uploadedFile.name);
-        }
-    });
-
-
-
-    var upload = multer({ storage: storage }).array('files', 3);
-
-    upload(req, res, function (err) {
-        if (err) {
-            log.debug("File Upload Err" + err);
-        }
-        /*if (err) deferred.reject();
-        else deferred.resolve(req.file.uploadedFile);
-        */
-
-        //log.debug(req.file.uploadedFile);
-
-        res.end();
-    });
-
-    //return deferred.promise;
-
+var fileuploadMulti = function(req, res) {
+  //log.debug("body ===", req);
+  //var deferred = Q.defer();
+  var storage = multer.diskStorage({
+    // 서버에 저장할 폴더
+    destination: function(req, file, cb) {
+      cb(null, "d:\\test");
+    },
+    // 서버에 저장할 파일 명
+    filename: function(req, file, cb) {
+      log.debug("file" + JSON.stringify(file));
+      file.uploadedFile = {
+        name: file.originalname
+      };
+      log.debug("filename=" + file.uploadedFile.name);
+      cb(null, file.uploadedFile.name);
+    }
+  });
+  var upload = multer({
+    storage: storage
+  }).array('files', 3);
+  upload(req, res, function(err) {
+    if(err) {
+      log.debug("File Upload Err" + err);
+    }
+    /*if (err) deferred.reject();
+    else deferred.resolve(req.file.uploadedFile);
+    */
+    //log.debug(req.file.uploadedFile);
+    res.end();
+  });
+  //return deferred.promise;
 };
-
 module.exports.fileuploadSingle = fileuploadSingle;
 module.exports.fileuploadMulti = fileuploadMulti;
-
