04d55186627164fe9a06c615f30306ab3719ddce
bkLove <chlqudrnr@gmail.com>
2020-01-02 13:58:36 +0900

    [common/tool] js 소스 팩킹

--- client/src/js/common/tool/numutil.js
+++ client/src/js/common/tool/numutil.js
@@ -1,31 +1,29 @@
-var numutil = {    
-
+var numutil = {
   getMinArr: function(arr) {
     var tarr = [];
     var val = 0;
-    for(let i=0; i < arr.length; i++) {
+    for(let i = 0; i < arr.length; i++) {
       if(arr[i] == 0) continue;
       if(i == 0) {
         val = arr[i];
-      }else {
+      } else {
         if(arr[i] < val) val = arr[i];
       }
     }
     return val;
   },
-
   getMaxArr: function(arr) {
     var tarr = [];
     var val = 0;
-    for(let i=0; i < arr.length; i++) {
+    for(let i = 0; i < arr.length; i++) {
       if(arr[i] == 0) continue;
       if(i == 0) {
         val = arr[i];
-      }else {
+      } else {
         if(arr[i] > val) val = arr[i];
       }
     }
     return val;
   },
-}  
+}
 export default numutil
--- client/src/js/common/tool/tool.js
+++ client/src/js/common/tool/tool.js
@@ -1,11 +1,8 @@
-var tool = {    
-
+var tool = {
   chkEmail: function(str) {
     var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-
-    if (regExp.test(str)) return true;
+    if(regExp.test(str)) return true;
     else return false;
-  
   },
-}  
+}
 export default tool
