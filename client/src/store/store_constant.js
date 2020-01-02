965f9c793f07e3164b27e7a0767d566aa7347d7b
bkLove <chlqudrnr@gmail.com>
2020-01-02 14:00:25 +0900

    [store] js 소스 팩킹

--- client/src/store/store_constant.js
+++ client/src/store/store_constant.js
@@ -1,22 +1,22 @@
 export default {
-    // 사용자 정보 등록
-    ADD_USER : "addUser",
-    DELETE_USER : "delUser",
-    // 날짜 정보관리
-    SET_BEF_DATES : "setBefDates",
-    // MAST 정보관리
-    SET_INDEX : "setIndexMast",
-    SET_ETP : "setEtpMast",
-    // 메일관리
-    // INSERT_MAIL : "insertMail",
-    // UPDATE_MAIL : "updateMail",
-    // DELETE_MAIL : "deleteMail",
-    // CLOSE_MAIL : "closeMail",
-    // SHOW_MAIL : "showMail",
-    // //공지사항 관리
-    // INSERT_STORY : "insertStory",
-    // UPDATE_STORY : "updateStory",
-    // DELETE_STORY : "deleteStory",
-    // CLOSE_STORY : "closeStory",
-    // SHOW_STORY : "showStory"
+  // 사용자 정보 등록
+  ADD_USER: "addUser",
+  DELETE_USER: "delUser",
+  // 날짜 정보관리
+  SET_BEF_DATES: "setBefDates",
+  // MAST 정보관리
+  SET_INDEX: "setIndexMast",
+  SET_ETP: "setEtpMast",
+  // 메일관리
+  // INSERT_MAIL : "insertMail",
+  // UPDATE_MAIL : "updateMail",
+  // DELETE_MAIL : "deleteMail",
+  // CLOSE_MAIL : "closeMail",
+  // SHOW_MAIL : "showMail",
+  // //공지사항 관리
+  // INSERT_STORY : "insertStory",
+  // UPDATE_STORY : "updateStory",
+  // DELETE_STORY : "deleteStory",
+  // CLOSE_STORY : "closeStory",
+  // SHOW_STORY : "showStory"
 }
